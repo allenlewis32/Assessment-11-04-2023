8dae3662f8d312b48d5f0f69c07c0b6b267d727e
Allen Lewis <allenlewis32@gmail.com>
2023-04-11 23:11:47 +0530

    Implemented CheckParentheses

--- Assessment-11-04-2023/Program.cs
+++ Assessment-11-04-2023/Program.cs
@@ -4,7 +4,30 @@
 	{
 		static void Main(string[] args)
 		{
-			
+			Console.WriteLine(CheckParentheses("(())()()"));
+			Console.WriteLine(CheckParentheses("(())()("));
+			Console.WriteLine(CheckParentheses("(())()("));
+			Console.WriteLine(CheckParentheses("(())())"));
+		}
+		
+		private static bool CheckParentheses(string str)
+		{
+			Stack<char> stack = new();
+			foreach (char c in str)
+			{
+				if (c == '(')
+				{
+					stack.Push(c);
+				}
+				else
+				{
+					if (stack.Count == 0 || stack.Pop() != '(')
+					{
+						return false;
+					}
+				}
+			}
+			return stack.Count == 0;
 		}
 	}
 }
