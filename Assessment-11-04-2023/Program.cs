4d191105b5378d0e2968a5361cf8efa197e9c429
Allen Lewis <allenlewis32@gmail.com>
2023-04-11 23:12:10 +0530

    Implemented GenerateParentheses

--- Assessment-11-04-2023/Program.cs
+++ Assessment-11-04-2023/Program.cs
@@ -8,8 +8,37 @@
 			Console.WriteLine(CheckParentheses("(())()("));
 			Console.WriteLine(CheckParentheses("(())()("));
 			Console.WriteLine(CheckParentheses("(())())"));
+
+			foreach (string str in GenerateParentheses(3))
+			{
+				Console.WriteLine(str);
+			}
 		}
-		
+		private static List<string> GenerateParentheses(int n)
+		{
+			List<string> result = new();
+			char[] ch = new char[n * 2]; // since there are n pairs(n*2)
+			GenerateParenthesesHelper(result, ch, 0, n, 0);
+			return result;
+		}
+		private static void GenerateParenthesesHelper(List<string> result, char[] ch, int pos, int toOpen, int toClose)
+		{
+			if (toOpen == 0 && toClose == 0) // if no more brackets to add, add ch to result
+			{
+				result.Add(new(ch));
+				return;
+			}
+			if(toOpen > 0) // if more pair needs to be opened, open it
+			{
+				ch[pos] = '(';
+				GenerateParenthesesHelper(result, ch, pos + 1, toOpen - 1, toClose + 1);
+			}
+			if(toClose > 0) // if there are pairs that need to be closed, close it
+			{
+				ch[pos] = ')';
+				GenerateParenthesesHelper(result, ch, pos + 1, toOpen, toClose - 1);
+			}
+		}
 		private static bool CheckParentheses(string str)
 		{
 			Stack<char> stack = new();
